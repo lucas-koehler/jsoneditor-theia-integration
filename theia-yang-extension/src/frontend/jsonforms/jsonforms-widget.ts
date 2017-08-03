e10cfd7bb222d3b145f28f9d48d486a8080253e2
Lucas Koehler <l.koehler93@gmail.com>
2017-08-03 22:55:00 +0200

    use EcoreEditor element

--- theia-yang-extension/src/frontend/jsonforms/jsonforms-widget.ts
+++ theia-yang-extension/src/frontend/jsonforms/jsonforms-widget.ts
@@ -1,45 +1,30 @@
 import { Widget } from "@phosphor/widgets";
 // import { JsonFormsElement } from 'jsonforms';
-import { JsonEditor } from 'jsoneditor-ma';
-import {imageProvider, labelProvider, modelMapping} from './ecore-config';
-import {ecore_schema} from './schema';
+import { EcoreEditor } from 'jsoneditor-ma';
+// import {imageProvider, labelProvider, modelMapping} from './ecore-config';
+// import {ecore_schema} from './schema';
 import 'jsoneditor-ma';
-
-// import 'jsonforms';
-// import {ControlElement} from 'jsonforms/dist/ts-build/models/uischema';
 
 let num = 0;
 export class JSONFormsWidget extends Widget {
 
-  private jsonEditor: JsonEditor;
+  // private jsonEditor: JsonEditor;
+  private ecoreEditor: EcoreEditor;
 
     constructor(data: object, private saveCallback: (data: Object) => void) {
         super();
-        // const jsonForms = document.createElement('json-forms') as JsonFormsElement;
-        this.jsonEditor = document.createElement('json-editor') as JsonEditor;
-        this.jsonEditor.classList.add('dark');
-        this.jsonEditor.setImageMapping(imageProvider);
-        this.jsonEditor.setLabelMapping(labelProvider);
-        this.jsonEditor.setModelMapping(modelMapping);
-        this.jsonEditor.schema = ecore_schema;
-        this.jsonEditor.data = data;
-        // jsonForms.data = data;
-        // jsonForms.uiSchema =
-        // {
-        //   'type': 'MasterDetailLayout',
-        //     'scope': {
-        //       '$ref': '#'
-        //     }
-        // } as ControlElement;
+        this.ecoreEditor = document.createElement('ecore-editor') as EcoreEditor;
+        this.ecoreEditor.classList.add('dark');
 
-        this.node.appendChild(this.jsonEditor);
+        this.ecoreEditor.data = data;
+        this.node.appendChild(this.ecoreEditor);
         this.node.style.overflowY = 'scroll';
         num++;
-        this.id = 'jsoneditor-' + num;
+        this.id = 'ecoreeditor-' + num;
     }
 
     close() {
       super.close();
-      this.saveCallback(this.jsonEditor.data);
+      this.saveCallback(this.ecoreEditor.data);
     }
 }
